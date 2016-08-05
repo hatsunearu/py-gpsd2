3e920e4ff85fd49850986c71e4c9948faaec6424
Martijn Braam <martijn@brixit.nl>
2016-08-05 10:14:29 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
         name='gpsd-py3',
-        version='0.1.0',
+        version='0.2.0',
         packages=['gpsd'],
         url='https://github.com/MartijnBraam/gpsd-py3',
         license='MIT',
